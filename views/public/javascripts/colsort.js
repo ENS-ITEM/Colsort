b31451a7f8f28a081cb609d7e036ef876261ccba
Daniel Berthereau <Daniel.github@Berthereau.net>
2020-04-13 00:00:00 +0200

    Fixed js to hide/show all.

--- views/public/javascripts/colsort.js
+++ views/public/javascripts/colsort.js
@@ -4,11 +4,12 @@ $(document).ready(function() {
         $(this).parent().nextAll('div.notices, div.collections').toggle();
     });
     $('.tout').click(function() {
-        $('div.notices, div.collections').toggle();
         if ($(this).html() == 'Tout replier') {
             $(this).html('Tout d&eacute;plier');
+            $('div.notices, div.collections').hide();
         } else {
             $(this).html('Tout replier');
+            $('div.notices, div.collections').show();
         }
     });
 });
